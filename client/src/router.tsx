93e0586467da8b3bfc721c7ace9963f498efe24b
Wayne Kinyanjui <waynekn24@gmail.com>
2025-05-06 02:20:42 +0300

    add route for user profile page

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router";
 
 import HomePage from "./pages/home";
 import AuthPage from "./pages/auth";
+import ProfilePage from "./pages/profilePage";
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="auth/callback/" element={<AuthPage />} />
+      <Route path="profile/:username/" element={<ProfilePage />} />
     </Routes>
   );
 }
