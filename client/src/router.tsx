2ddc389dcff5b99f27000dafaa1a0289a06119cb
Wayne Kinyanjui <waynekn24@gmail.com>
2025-05-20 00:44:08 +0300

    add route for creating a new spreadsheet

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router";
 import HomePage from "./pages/home";
 import AuthPage from "./pages/auth";
 import ProfilePage from "./pages/profilePage";
+import CreateSheet from "./pages/createSpreadSheet";
 
 function Router() {
   return (
@@ -10,6 +11,7 @@ function Router() {
       <Route path="/" element={<HomePage />} />
       <Route path="auth/callback/" element={<AuthPage />} />
       <Route path="profile/:username/" element={<ProfilePage />} />
+      <Route path="spreadsheet/create/" element={<CreateSheet />} />
     </Routes>
   );
 }
