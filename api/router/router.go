03a7322f6cb715ba288641723a666fa5bfc770ae
Wayne Kinyanjui <waynekn24@gmail.com>
2025-05-22 16:23:24 +0300

    refactor: pass database connection to router initialization

--- api/router/router.go
+++ api/router/router.go
@@ -1,12 +1,14 @@
 package router
 
 import (
+	"database/sql"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 // New registers the routes and returns the router.
-func New() *gin.Engine {
+func New(db *sql.DB) *gin.Engine {
 	r := gin.Default()
 	config := cors.DefaultConfig()
 	config.AllowOrigins = []string{"http://localhost:5173"}
