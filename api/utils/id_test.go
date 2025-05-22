a82b5447b17299d02656cc3a1098ca56418306b7
Wayne Kinyanjui <waynekn24@gmail.com>
2025-05-22 17:38:31 +0300

    test generated id only contains base 62 characters

--- api/utils/id_test.go
+++ api/utils/id_test.go
@@ -1,11 +1,22 @@
 package utils
 
-import "testing"
+import (
+	"regexp"
+	"testing"
+)
 
 // TestGenerateId tests the GenerateID function to ensure it generates a unique ID
 // of the expected length.
 func TestGenerateId(t *testing.T) {
 	id := GenerateID()
+
+	base62Regex := regexp.MustCompile(`^[` + regexp.QuoteMeta(base62Alphabet) + `]+$`)
+	isValid := base62Regex.MatchString(id)
+
+	if !isValid {
+		t.Errorf("Expected ID to only contain base62 chars [%s], got %s", base62Alphabet, id)
+	}
+
 	if len(id) > maxIdLength {
 		t.Errorf("Expected ID length lesser than or equal %d, got %d", maxIdLength, len(id))
 	}
