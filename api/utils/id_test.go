a965fab4eae9c0cebeb52582fee5da62257a4e22
Wayne Kinyanjui <waynekn24@gmail.com>
2025-05-22 17:13:37 +0300

    use maxIdLength constant in `TestGenerateId`

--- api/utils/id_test.go
+++ api/utils/id_test.go
@@ -6,8 +6,8 @@ import "testing"
 // of the expected length.
 func TestGenerateId(t *testing.T) {
 	id := GenerateID()
-	if len(id) > 22 {
-		t.Errorf("Expected ID length lesser than or equal 22, got %d", len(id))
+	if len(id) > maxIdLength {
+		t.Errorf("Expected ID length lesser than or equal %d, got %d", maxIdLength, len(id))
 	}
 
 }
