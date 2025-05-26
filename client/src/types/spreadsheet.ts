f52944af19ae515ac4465d824accffb73f4d7820
Wayne Kinyanjui <waynekn24@gmail.com>
2025-05-26 17:18:09 +0300

    define `Spreadsheet` type to represent spreadsheet data retrieved from the API

--- client/src/types/spreadsheet.ts
+++ client/src/types/spreadsheet.ts
@@ -11,3 +11,15 @@ export type SpreadsheetInitErr = Omit<Partial<SpreadsheetInit>, "colTitles"> & {
   error?: string;
   colTitles?: string;
 };
+
+// Represents a spreadsheet retrieved from the api
+export type Spreadsheet = {
+  id: string;
+  title: string;
+  description: string;
+  owner: string;
+  createdAt: string;
+  updatedAt: string;
+  data: string[][];
+  deadline: string;
+};
