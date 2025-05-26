b9b6ce3d4d038a7fd7e5e2c6b872ced8661f196a
Wayne Kinyanjui <waynekn24@gmail.com>
2025-05-26 18:13:27 +0300

    define `Spreadsheet` type to represent spreadsheet data stored in the database

--- api/models/spreadsheet.go
+++ api/models/spreadsheet.go
@@ -9,3 +9,15 @@ type SpreadsheetInit struct {
 	Deadline    time.Time `json:"deadline" time_format:"2006-01-02T15:04:05Z07:00" binding:"required"` // Deadline in RFC3339 format
 	ColTitles   []string  `json:"colTitles" binding:"required,min=1"`
 }
+
+// Spreadsheet represents a spreadsheet stored in the database.
+type Spreadsheet struct {
+	ID          string    `json:"id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Owner       string    `json:"owner"`
+	CreatedAt   time.Time `json:"createdAt"`
+	UpdatedAt   time.Time `json:"updatedAt"`
+	Data        []byte    `json:"data"` // [][]string stored as jsonb
+	Deadline    time.Time `json:"deadline"`
+}
