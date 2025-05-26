d2b802d77a0660c11d040f01f2df969c225ab873
Wayne Kinyanjui <waynekn24@gmail.com>
2025-05-26 17:57:39 +0300

    add functionality to fetch and display a users spreadsheets

--- client/src/pages/profilePage.tsx
+++ client/src/pages/profilePage.tsx
@@ -1,5 +1,11 @@
+import { useEffect, useState } from "react";
+import { AxiosError } from "axios";
+import { Link } from "react-router";
 import { User } from "lucide-react";
-import { withAuthenticationRequired } from "react-oidc-context";
+import { useQuery } from "@tanstack/react-query";
+import { useAuth, withAuthenticationRequired } from "react-oidc-context";
+
+import { useApi } from "@/hooks/api";
 
 import {
   Popover,
@@ -8,9 +14,41 @@ import {
 } from "@/components/ui/popover";
 import { Button } from "@/components/ui/button";
 import UserInfoCard from "@/components/cards/userInfo";
-import { Link } from "react-router";
+import SheetList from "@/components/sheetList";
+import ListSkeleton from "@/skeletons/listSkeleton";
+
+import { GenericApiError } from "@/types/error";
+import { Spreadsheet } from "@/types/spreadsheet";
 
 function ProfilePage() {
+  const auth = useAuth();
+  const api = useApi();
+  const [sheets, setSheets] = useState<Spreadsheet[]>([]);
+  const [errMsg, setErrMsg] = useState<string>("");
+
+  const { data, error, isSuccess, isPending, isError } = useQuery({
+    queryKey: [`${auth.user?.profile.sub}`],
+    queryFn: () => api.get<Spreadsheet[]>("spreadsheets/"),
+    retry: (failureCount, error) => {
+      const apiError = error as AxiosError;
+      if (apiError.response?.status === 500) return false; // No retry on server errors
+      return failureCount < 3;
+    },
+    staleTime: 1000 * 60 * 30, // 30 minutes
+  });
+
+  useEffect(() => {
+    if (isSuccess) {
+      setSheets(data.data);
+      return;
+    }
+
+    if (isError) {
+      const err = error as AxiosError<GenericApiError>;
+      setErrMsg(err.response?.data.error || "An unexpected error occurred");
+    }
+  }, [data, error, isError, isSuccess]);
+
   return (
     <div className="h-screen w-screen flex flex-col md:flex-row bg-gray-100">
       <main className="flex-1 p-4 md:p-8 sm:text-sm md:text-base">
@@ -29,16 +67,36 @@ function ProfilePage() {
           </PopoverContent>
         </Popover>
 
-        <section className="w-full max-w-3xl mx-auto border-2 border-gray-300 rounded-lg bg-white shadow-lg p-8 space-y-6">
+        <section className="w-full max-w-3xl mx-auto border border-gray-200 rounded-xl bg-white shadow-md p-10 space-y-8">
           <div>
             <Link
               to="../spreadsheet/create/"
-              className="text-black underline hover:text-gray-700"
+              className="inline-block text-blue-600 font-semibold underline hover:text-blue-800 transition-colors duration-200"
             >
-              Create spreadsheet
+              Create a New Spreadsheet
             </Link>
           </div>
-          {/* TODO */}
+
+          {isPending && <ListSkeleton />}
+
+          {isError && (
+            <div
+              role="alert"
+              className="bg-red-600 text-white rounded-lg py-4 px-2"
+            >
+              {errMsg || "An unexpected error occurred. Please try again."}
+            </div>
+          )}
+
+          {isSuccess && (
+            <div>
+              {sheets.length > 0 ? (
+                <SheetList sheets={sheets} />
+              ) : (
+                <p className="text-gray-500">No spreadsheets found.</p>
+              )}
+            </div>
+          )}
         </section>
       </main>
     </div>
