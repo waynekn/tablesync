d16499063866684987a138b1cdcd19db8f1bac0a
Wayne Kinyanjui <waynekn24@gmail.com>
2025-06-10 22:48:25 +0300

    update function to accept redis client and pass client to auth middleware

--- api/router/router.go
+++ api/router/router.go
@@ -5,13 +5,14 @@ import (
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
 	"github.com/waynekn/tablesync/api/db/repo"
 	"github.com/waynekn/tablesync/api/handlers"
 	"github.com/waynekn/tablesync/api/middleware"
 )
 
 // New registers the routes and returns the router.
-func New(db *sql.DB) *gin.Engine {
+func New(db *sql.DB, rdb *redis.Client) *gin.Engine {
 	r := gin.Default()
 	config := cors.DefaultConfig()
 	config.AllowOrigins = []string{"http://localhost:5173"}
@@ -26,8 +27,8 @@ func New(db *sql.DB) *gin.Engine {
 	wsRepo := repo.NewWsRepo(db)
 	wsHandler := handlers.NewWsHandler(wsRepo)
 
-	r.POST("spreadsheet/create/", middleware.RequireAuth(), spreadsheetHandler.CreateSpreadsheetHandler)
-	r.GET("spreadsheets/", middleware.RequireAuth(), spreadsheetHandler.GetOwnSpreadsheetsHandler)
+	r.POST("spreadsheet/create/", middleware.RequireAuth(rdb), spreadsheetHandler.CreateSpreadsheetHandler)
+	r.GET("spreadsheets/", middleware.RequireAuth(rdb), spreadsheetHandler.GetOwnSpreadsheetsHandler)
 
 	// websocket routes
 	r.GET("ws/sheet/:sheetID/edit/", wsHandler.EditSessionHandler)
