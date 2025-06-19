32846ce2ad49c0152e90c82c378ea4c6bd10470f
Wayne Kinyanjui <waynekn24@gmail.com>
2025-06-20 01:15:19 +0300

    add tests for `InitRedisSheet` method

--- core/collab/redis_store_test.go
+++ core/collab/redis_store_test.go
@@ -57,3 +57,45 @@ func TestSheetExists_WhenSheetExists(t *testing.T) {
 	assert.NoError(t, err, "should not return an error when checking for an existing sheet")
 	assert.True(t, exists, "should return true for an existing sheet")
 }
+
+func TestInitRedisSheet(t *testing.T) {
+	sheetID := utils.GenerateID()
+	sheetDeadline := time.Now().Add(10 * time.Minute)
+	sheetData := &[][]string{
+		{"A1", "B1"},
+		{"A2", "B2"},
+	}
+
+	err := testStore.InitRedisSheet(sheetID, sheetDeadline, sheetData)
+	assert.NoError(t, err, "should not return an error when initializing a sheet with a valid deadline")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	// Check that the key exists
+	exists, err := testStore.rdb.Exists(ctx, sheetID).Result()
+	assert.NoError(t, err)
+	assert.Equal(t, int64(1), exists, "sheet key should exist in Redis")
+
+	// Check TTL is set
+	ttl, err := testStore.rdb.TTL(ctx, sheetID).Result()
+	assert.NoError(t, err, "should not return an error when checking TTL")
+	expectedTTL := 15 * time.Minute
+	assert.Equal(t, ttl, expectedTTL)
+
+	// Check that the correct data is stored
+	result, err := testStore.rdb.HGetAll(ctx, sheetID).Result()
+	assert.NoError(t, err)
+	assert.Equal(t, 4, len(result), "should store 4 cells for a 2x2 sheet")
+
+	expected := map[string]string{
+		"0:0": "A1",
+		"0:1": "B1",
+		"1:0": "A2",
+		"1:1": "B2",
+	}
+
+	for k, v := range expected {
+		assert.Equal(t, v, result[k], "value mismatch for cell %s", k)
+	}
+}
