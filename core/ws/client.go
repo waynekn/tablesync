9798580223e45d478fe50998389236532151b3b1
Wayne Kinyanjui <waynekn24@gmail.com>
2025-06-23 22:44:26 +0300

    refactor `NewClient` to enable instantiation of `Client` with collabStore and hub

--- core/ws/client.go
+++ core/ws/client.go
@@ -1,12 +1,17 @@
 package ws
 
-import "github.com/gorilla/websocket"
+import (
+	"github.com/gorilla/websocket"
+	"github.com/waynekn/tablesync/core/collab"
+)
 
 // NewClient instantiates and returns a new Client
-func NewClient(sheetID string, conn *websocket.Conn) *Client {
+func NewClient(sheetID string, conn *websocket.Conn, collabStore *collab.Store, hub *Hub) *Client {
 	return &Client{
-		Conn:    conn,
-		SheetID: sheetID,
-		Send:    make(chan []byte, 50),
+		Conn:        conn,
+		SheetID:     sheetID,
+		Send:        make(chan []byte, 50),
+		collabStore: collabStore,
+		hub:         hub,
 	}
 }
