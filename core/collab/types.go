5631c88888c5fb3ab226d2148f49ba667151d81e
Wayne Kinyanjui <waynekn24@gmail.com>
2025-06-24 17:11:24 +0300

    move `EditMsg` and `BroadCastMsg` structs to collab package

---
+++ core/collab/types.go
@@ -0,0 +1,18 @@
+package collab
+
+// EditMsg carries the details of a spreadsheet cell edit
+// made by a client, for broadcast to other collaborators.
+type EditMsg struct {
+	Row  int    `json:"row"`
+	Col  int    `json:"col"`
+	Data string `json:"data"`
+}
+
+// BroadCastMsg represents a message to be broadcasted to sheet collaborators.
+// It has a SheetID field to identify the sheet whose clients should receive the
+// message and a Msg field, which is an EditMsg, holding the data that will be
+// broadcasted.
+type BroadCastMsg struct {
+	SheetID string
+	Msg     EditMsg
+}
--- core/ws/types.go
+++ core/ws/types.go
@@ -21,22 +21,5 @@ type Hub struct {
 	Clients    map[string][]*Client // map[sheetID]clients
 	Register   chan *Client
 	Unregister chan *Client
-	Broadcast  chan BroadCastMsg
-}
-
-// EditMsg carries the details of a spreadsheet cell edit
-// made by a client, for broadcast to other collaborators.
-type EditMsg struct {
-	Row  int    `json:"row"`
-	Col  int    `json:"col"`
-	Data string `json:"data"`
-}
-
-// BroadCastMsg represents a message to be broadcasted to sheet collaborators.
-// It has a SheetID field to identify the sheet whose clients should receive the
-// message and a Msg field, which is an EditMsg, holding the data that will be
-// broadcasted.
-type BroadCastMsg struct {
-	SheetID string
-	Msg     EditMsg
+	Broadcast  chan collab.BroadCastMsg
 }
